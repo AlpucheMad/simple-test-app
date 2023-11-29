ea649677c9235b75f52ce5541fd8f2d4e6728679
Gerardo Alpuche <alpuchebado@outlook.com>
2023-11-28 18:26:29 -0600

    Added project
    Has 3 tests to run after in jenkins

---
+++ src/main/java/com/alpuche/simpletestapp/SimpleTestAppApplication.java
@@ -0,0 +1,13 @@
+package com.alpuche.simpletestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleTestAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alpuche/simpletestapp/SimpleTestAppApplicationTests.java
@@ -0,0 +1,58 @@
+package com.alpuche.simpletestapp;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.junit.Assert.assertNotNull;
+
+@SpringBootTest
+class SimpleTestAppApplicationTests {
+
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	private MockMvc mockMvc;
+
+	@BeforeEach
+	void setUp() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+
+	@Test
+	void contextLoads() {
+		System.out.println("STARTING TESTS...");
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {"property1", "property2"})
+	void testProperties(@Value("${propertyName}") String propertyValue) {
+		assertNotNull(propertyValue);
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {"/endpoint1", "/endpoint2"})
+	void testEndpoints(String url) throws Exception {
+		mockMvc.perform(get(url)).andExpect(status().isOk());
+	}
+
+	@ParameterizedTest
+	@CsvSource({"1, 2, 3", "2, 3, 5", "3, 5, 8"})
+	void testAddition(int num1, int num2, int expectedResult) {
+		assertEquals(expectedResult, num1 + num2);
+	}
+
+}
